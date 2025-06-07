ae86d9a1e55d70dca72825d35d86022f573dbec6
あゆむ <168393409+amu1017@users.noreply.github.com>
2025-06-08 03:01:54 +0900

    デプロイ処理を変更

--- manual-setup.sh
+++ manual-setup.sh
@@ -5,6 +5,8 @@ DEPLOY_DIR='/etc/nginx/conf.d/handson-test'
 CONF_NAME='websv.conf'
 
 echo "--- Starting Nginx setup ---"
+SCRIPT_DIR=$(dirname "$0")
+pushd "${SCRIPT_DIR}" >/dev/null
 apt update && apt install -y nginx
 systemctl stop nginx
 
@@ -20,6 +22,8 @@ sed -i "s@PLEASE_REPLACE_DEPLOY_DIR@${DEPLOY_DIR}@g" ${DEPLOY_DIR}/${CONF_NAME}
 
 
 echo "--- Setting up symbolic links and services ---"
+[ -L /etc/nginx/conf.d/${CONF_NAME} ] && unlink /etc/nginx/conf.d/${CONF_NAME}
 ln -s ${DEPLOY_DIR}/${CONF_NAME} /etc/nginx/conf.d/${CONF_NAME}
 find /etc/nginx/sites-enabled -type l -delete
 systemctl enable --now nginx
+popd >/dev/null
